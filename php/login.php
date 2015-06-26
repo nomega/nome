3217a9e9cbcd620c486b134ede6369f77752b5a4
Hansu <daktwigim@gmail.com>
2015-06-26 16:11:09 +0900

    로그인 기능 완

--- php/login.php
+++ php/login.php
@@ -1,4 +1,5 @@
 <?php
+
 	$conn = mysqli_connect("localhost","root","admin01");
 	mysqli_select_db($conn,"loginpage");
 
@@ -7,17 +8,17 @@
 	if($idsearch_result->num_rows == "1") {
 
 		$sql_search = "SELECT * FROM user WHERE u_id='".$_POST['user_id']."' AND u_pw='".$_POST['user_pw']."'";
-		$search_result = mysql_query($conn,$sql_search);
+		$search_result = mysqli_query($conn,$sql_search);
 		
 		if($search_result->num_rows == "0") {
 			header('location: /passwordfail.php');
 		} else {
 			header('location: /success.php');
 		}
-	else {
-		$sql_insert = "INSERT INTO `user`(`u_id`,`u_pw`,`u_name`,`u_mail`) VALUES(".$_POST['user_id'].",".$_POST['user_pw'].",'사용자','user@mail.com')";
+	} else {
+		$sql_insert = "INSERT INTO user(u_id,u_pw,u_name,u_mail) VALUES('".$_POST['user_id']."','".$_POST['user_pw']."','any','user@mail.com')";
 		$insert_result = mysqli_query($conn,$sql_insert);
-		header('location: /success.php');
+		header('location: /signin.php');
 	}
 
 ?>
