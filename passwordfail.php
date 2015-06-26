1725ef6d1592405a6edd6097aa799043aa525d93
wychoi0709 <wychoi0709@gmail.com>
2015-06-26 12:27:28 +0900

    1. html파일 fail, success, passwordfail 추가 2. js 파일 추가중

---
+++ passwordfail.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="ko">
+  <head>
+      <title>암호 틀림</title>
+
+      <!-- Customizing CSS/js -->
+      <script type="text/javascript" src="js/custom/closebrowser.js" ></script>
+      <link rel="stylesheet" type="text/css" href="">
+
+      <script type="text/javascript">
+        window.onload = closebrowser;
+      </script>
+      <!-- //Customizing CSS/js -->
+
+      <!-- Common meta tag -->
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <!-- //Common meta tag -->
+
+      <!-- Bootstrap -->
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link href="css/bootstrap.min.css" rel="stylesheet">    
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+      <script src="js/bootstrap.min.js"></script>
+      <!-- //Bootstrap -->
+  </head>
+  <body>
+    <nav>
+      <h5>암호가 틀렸습니다</h5>
+    </nav>
+  </body>
+</html>
---
+++ success.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="ko">
+	<head>
+	    <title>로그인 성공</title>
+
+	    <!-- Customizing CSS/js -->
+	    <script type="text/javascript" ></script>
+	    <link rel="stylesheet" type="text/css" href="">
+	    <!-- //Customizing CSS/js -->
+
+	    <!-- Common meta tag -->
+	    <meta charset="utf-8">
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	    <!-- //Common meta tag -->
+
+	    <!-- Bootstrap -->
+	    <meta name="viewport" content="width=device-width, initial-scale=1">
+	    <link href="css/bootstrap.min.css" rel="stylesheet">    
+	    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	    <script src="js/bootstrap.min.js"></script>
+	    <!-- //Bootstrap -->
+	</head>
+	<body>
+		<nav>
+  			<h5>로그인에 성공하셨습니다</h5>
+  		</nav>
+  		<div class="wrapper">
+	      	<form action="/php/login.php" method="POST">
+	        	<p>ID <input type="text" name="user_id"></p>
+	        	<p>PW <input type="text" name="user_pw"></p>
+	        	<input value ="로그인하기"type="submit">
+	      	</form>
+	  	</div>
+	</body>
+</html>
