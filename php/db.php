dca50c2482f47dde8157458837fe8392edfa04a1
Hansu <daktwigim@gmail.com>
2015-06-26 16:33:46 +0900

    db.php 실행하면 db 자동 추가

---
+++ php/db.php
@@ -0,0 +1,21 @@
+<?php
+	$conn = mysqli_connect("localhost","root","admin");
+	$sql_db = "CREATE DATABASE `loginpage`";
+	mysqli_query($conn,$sql_db);
+	mysqli_select_db($conn,'loginpage');
+	$sql_table = "CREATE TABLE `user` (
+		`id` int(11) NOT NULL AUTO_INCREMENT,
+		  `u_id` varchar(30) NOT NULL,
+		  `u_pw` vachar(100) NOT NULL,
+		  `u_name` varchar(30) NOT NULL,
+		  `u_mail` varchar(100) NOT NULL,
+		  PRIMARY KEY (id)
+		) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
+	mysqli_query($conn,$sql_table);
+	$sql_insert = "INSERT INTO user(u_id,u_pw,u_name,u_mail) values('tester','0000','user','user@mail.com')";
+	mysqli_query($conn,$sql_insert);
+	$result = mysqli_query($conn,"SELECT * FROM user");
+	$row = mysqli_fetch_assoc($result);
+	echo "DB확인<br />";
+	echo $row['id'].",".$row['u_id'].",".$row['u_pw'].",".$row['u_name'].",".$row['u_mail'];
+?>
