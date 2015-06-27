1ca342155093788876cf476e7e4c795b71ac531d
wychoi0709 <wychoi0709@gmail.com>
2015-06-27 14:00:04 +0900

    localhost에서 3300 하나 뺌

--- php/login.php
+++ php/login.php
@@ -1,6 +1,6 @@
 <?php
 
-	$conn = mysqli_connect("localhost:3300","root","admin01");
+	$conn = mysqli_connect("localhost","root","admin01");
 	mysqli_select_db($conn,"loginpage");
 	if($conn == "FALSE") {
 		$conn = mysql_connect("localhost:3300","root","admin01");
