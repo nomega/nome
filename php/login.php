061735a113be13602e33d1a8676054e23c5f543c
wychoi0709 <wychoi0709@gmail.com>
2015-07-01 11:44:08 +0900

    뭐가 바뀐지 잘 모름

--- php/login.php
+++ php/login.php
@@ -1,8 +1,8 @@
 <?php
 
 	$conn = mysqli_connect("localhost","root","admin01");
-	if($conn == "FALSE") {
-		$conn = mysqli_connect("localhost","root","admin01","3300");
+	if(!$conn) {
+		$conn = mysqli_connect("localhost:3300","root","admin01");
 	}
 	mysqli_select_db($conn,"loginpage");
 	$sql_idsearch = "SELECT * FROM user WHERE u_id='".$_POST['user_id']."'";
@@ -20,7 +20,7 @@
 	} else {
 		$sql_insert = "INSERT INTO user(u_id,u_pw,u_name,u_mail) VALUES('".$_POST['user_id']."','".$_POST['user_pw']."','any','user@mail.com')";
 		$insert_result = mysqli_query($conn,$sql_insert);
-		/*header('location: /signin.php');*/
+		header('location: /signin.php');
 	}
 
 ?>
